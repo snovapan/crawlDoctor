779d072716292b0c9cb986127d9cd70b2c85e765
pan <pan.wj@deepin.asus>
2018-09-25 10:08:38 +0800

    添加Mysql数据库导入

--- README.md
+++ README.md
@@ -15,9 +15,22 @@
 
 #### 使用说明
 
-1. 安装python和scrapy
-2. cd crawlDoctor/
-3. python begin.py
+1. 安装python、scrapy、PyMySQL
+2. mysql建表：
+DROP TABLE IF EXISTS `t_doctor`;
+CREATE TABLE `t_doctor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `hospitalname` varchar(100) DEFAULT NULL COMMENT '医院名称',
+  `bigdept` varchar(100) DEFAULT NULL COMMENT '大科室',
+  `smalldept` varchar(100) DEFAULT NULL COMMENT '小科室',
+  `name` varchar(100) DEFAULT NULL COMMENT '姓名',
+  `title` varchar(100) DEFAULT NULL COMMENT '职称',
+  `introduction` varchar(5000) DEFAULT NULL COMMENT '简介',
+  `deptlink` varchar(1000) DEFAULT NULL COMMENT '科室链接',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+3. cd crawlDoctor/
+4. python3 begin.py
 
 #### 参与贡献
 
--- crawlDoctor/pipelines.py
+++ crawlDoctor/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
+import  pymysql
 
 
 class CrawldoctorPipeline(object):
@@ -15,9 +16,36 @@ class CrawldoctorPipeline(object):
 
     def process_item(self, item, spider):
         text = json.dumps(dict(item), ensure_ascii=False) + ",\n"
-        print(text.encode("utf-8"))
         self.filename.write(text.encode("utf-8"))
+        self.insert(item)
         return item
 
     def close_spider(self, spider):
         self.filename.close()
+
+
+    def getCon(self):
+        return pymysql.connect(host='127.0.0.1',
+                             user='root',
+                             password='root',
+                             db='test',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+    def insert(self,item):
+        conn = self.getCon()
+        try:
+            with conn.cursor() as cursor:
+                val = [item['hospitalname'],item['bigdept'],item['smalldept'],item['name'],item['title'],item['introduction'],item['deptlink']]
+                sql = 'INSERT INTO `t_doctor`(`hospitalname`, `bigdept`, `smalldept`, `name`, `title`, `introduction`, `deptlink`) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+                cursor.execute(sql, val)
+                # 如果没有设置自动提交事务，则这里需要手动提交一次
+                conn.commit()
+        except:
+            import traceback
+            traceback.print_exc()
+            # 发生错误时会滚
+            conn.rollback()
+        finally:
+            # 关闭数据库连接
+            conn.close()
